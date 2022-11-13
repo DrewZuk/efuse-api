08af9dd2ee60598997596b679c2bb9e66a4b5474
Andrew Zuk <azuk040891@gmail.com>
2022-11-13 10:45:50 -0500

    [phase-0] adds health endpoint, removes root app service + controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule, HealthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
